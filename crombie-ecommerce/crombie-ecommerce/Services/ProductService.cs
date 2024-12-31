8619caf7ff860ca44f3f1151bb5d82287d8be61d
Adel Fetner <adelfetner@gmail.com>
2024-12-31 16:52:37 -0300

    chore: add get products

---
+++ crombie-ecommerce/crombie-ecommerce/Services/ProductService.cs
@@ -0,0 +1,21 @@
+﻿using crombie_ecommerce.Contexts;
+using crombie_ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace crombie_ecommerce.Services
+{
+    public class ProductService
+    {
+        private readonly ShopContext _context;
+
+        public ProductService(ShopContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Product>> GetAllProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+    }
+}
