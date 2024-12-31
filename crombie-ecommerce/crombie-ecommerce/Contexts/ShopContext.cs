35795da4abebde872bcb921894c8055444bf9a77
Adel Fetner <adelfetner@gmail.com>
2024-12-30 21:19:39 -0300

    feat: Add context, connectionstring, and initial migration

---
+++ crombie-ecommerce/crombie-ecommerce/Contexts/ShopContext.cs
@@ -0,0 +1,83 @@
+﻿using crombie_ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace crombie_ecommerce.Contexts
+{
+    public class ShopContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Wishlist> Wishlists { get; set; }
+
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // builder for user entity
+            modelBuilder.Entity<User>(user =>
+            {
+                user.ToTable("User");
+                user.HasKey(u => u.UserId);
+                user.Property(u => u.Name).IsRequired().HasMaxLength(50);
+                user.Property(u => u.Email).IsRequired();
+                user.Property(u => u.Password).IsRequired().HasMaxLength(100);
+                user.Property(u => u.IsVerified).HasDefaultValue(false);
+
+                // user to wl has a one to one relationship
+                user.HasOne(u => u.Wishlist)
+                    .WithOne(w => w.User)
+                    .HasForeignKey<Wishlist>(w => w.UserId)
+                    .OnDelete(DeleteBehavior.Cascade);
+
+                user.HasOne(u => u.Product)
+                    .WithOne(p => p.User)
+                    .HasForeignKey<User>(u => u.ProductId);
+            });
+
+            // builder for product entity
+            modelBuilder.Entity<Product>(product =>
+            {
+                product.ToTable("Product");
+                product.HasKey(p => p.Id);
+                product.Property(p => p.Name).IsRequired().HasMaxLength(50);
+                product.Property(p => p.Description).HasMaxLength(100); 
+                product.Property(p => p.Price).HasColumnType("decimal(18,2)");
+                product.Property(p => p.Brand).HasMaxLength(50);
+                product.Property(p => p.Category).HasMaxLength(50);
+
+                // product to user
+                product.HasOne(p => p.User)
+                    .WithOne(u => u.Product)
+                    .HasForeignKey<User>(u => u.UserId);
+
+                // product to wl
+                product.HasOne(p => p.Wishlist)
+                    .WithOne(w => w.Product)
+                    .HasForeignKey<Wishlist>(w => w.ProductId);
+            });
+
+            // builder for wishlist entity
+            modelBuilder.Entity<Wishlist>(wishlist =>
+            {
+                wishlist.ToTable("Wishlist");
+                wishlist.HasKey(w => w.WishlistId);
+                wishlist.Property(w => w.Name).IsRequired().HasMaxLength(50);
+                wishlist.Property(w => w.Tag).IsRequired().HasMaxLength(100);
+
+                // wl to user has a one to one relationship
+                wishlist.HasOne(w => w.User)
+                        .WithOne(u => u.Wishlist)
+                        .HasForeignKey<Wishlist>(w => w.UserId)
+                        .OnDelete(DeleteBehavior.NoAction);
+
+                // wl to product has a  one to one relationship
+                wishlist.HasOne(w => w.Product)
+                        .WithOne(p => p.Wishlist)
+                        .HasForeignKey<Wishlist>(w => w.ProductId);
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ crombie-ecommerce/crombie-ecommerce/Migrations/20241231001743_InitialCreate.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace crombie_ecommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Brand = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    WishlistId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    IsVerified = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    WishlistId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_User_Product_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Wishlist",
+                columns: table => new
+                {
+                    WishlistId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Tag = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Wishlist", x => x.WishlistId);
+                    table.ForeignKey(
+                        name: "FK_Wishlist_Product_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Product",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Wishlist_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "UserId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Wishlist_ProductId",
+                table: "Wishlist",
+                column: "ProductId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Wishlist_UserId",
+                table: "Wishlist",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Wishlist");
+
+            migrationBuilder.DropTable(
+                name: "User");
+
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
