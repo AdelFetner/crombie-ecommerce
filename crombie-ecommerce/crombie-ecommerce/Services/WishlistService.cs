a526ac0ba657013ef95a8cb8c650e7449d64ab3b
Jeremias <schujmanjeremias05@gmail.com>
2024-12-31 16:58:32 -0300

    refact: added async methods

--- crombie-ecommerce/crombie-ecommerce/Services/WishlistService.cs
+++ crombie-ecommerce/crombie-ecommerce/Services/WishlistService.cs
@@ -18,47 +18,80 @@ namespace crombie_ecommerce.Services
         }
 
         //Method to get all wishlist
-        public List<Wishlist> GetAllWishlists()
+        public async Task<List<Wishlist>> GetAllWishlists()
         {
-            return _context.Wishlists.ToList();
+            return await _context.Wishlists.ToListAsync();
         }
+
+        //Second method to get all wishlist (ask if this is necessary)
+        //public List<Wishlist> GetAllWishlists()
+        //{
+        //    return _context.Wishlists.ToList();
+        //}
 
         //Method to get wishlist by ID
-        public List<Wishlist> GetWishlistByUserId(Guid userId)
+        public async Task<Wishlist?> GetWishlistById(Guid wishlistId)
         {
-            return _context.Wishlists.Where(w => w.UserId == userId).ToList();
+            return await _context.Wishlists.FindAsync(wishlistId);
         }
 
-        //Second method to get wishlist by id (ask if this is necessary)
+        //Second method to get wishlist by ID (ask if this is necessary)
+        //public List<Wishlist> GetWishlistByUserId(Guid userId)
+        //{
+        //    return _context.Wishlists.Where(w => w.UserId == userId).ToList();
+        //}
+
+        //Third method to get wishlist by id (ask if this is necessary)
         //public Wishlist? GetWishlistByUserId(Guid userId)
         //{
         //    return _context.Wishlists.SingleOrDefault(w => w.UserId == userId);
         //}
 
         //Method to create a new wishlist
-        public List<Wishlist> CreateWishlist(Wishlist wishlist)
+        public async Task<List<Wishlist>> CreateWishlist(Wishlist wishlist)
         {
             _context.Wishlists.Add(wishlist);
-            _context.SaveChanges();
-            return _context.Wishlists.ToList();
+            await _context.SaveChangesAsync();
+            return await _context.Wishlists.ToListAsync();
         }
 
+        // Second method to create a new wishlist (ask if this is necessary)
+        //public List<Wishlist> CreateWishlist(Wishlist wishlist)
+        //{
+        //    _context.Wishlists.Add(wishlist);
+        //    _context.SaveChanges();
+        //    return _context.Wishlists.ToList();
+        //}
+
         //Method to delete a wishlist
-        public List<Wishlist> DeleteWishlist(Guid wishlistId)
+        public async Task<List<Wishlist>> DeleteWishlist(Guid wishlistId)
         {
-            var wishlist = _context.Wishlists.Find(wishlistId);
+            var wishlist = await _context.Wishlists.FindAsync(wishlistId);
             if (wishlist == null)
             {
                 return null;
-
-                //(ask if this is necessary)
-                //throw new KeyNotFoundException($"Wishlist with ID {wishlistId} not found.");
             }
-
             _context.Wishlists.Remove(wishlist);
-            _context.SaveChanges();
-
-            return _context.Wishlists.ToList();
+            await _context.SaveChangesAsync();
+            return await _context.Wishlists.ToListAsync();
         }
+
+        //Second method to delete a wishlist (ask if this is necessary)
+        //public List<Wishlist> DeleteWishlist(Guid wishlistId)
+        //{
+        //    var wishlist = _context.Wishlists.Find(wishlistId);
+        //    if (wishlist == null)
+        //    {
+        //        return null;
+
+        //(ask if this is necessary)
+        //throw new KeyNotFoundException($"Wishlist with ID {wishlistId} not found.");
+        //    }
+
+        //    _context.Wishlists.Remove(wishlist);
+        //    _context.SaveChanges();
+
+        //    return _context.Wishlists.ToList();
+        //}
     }
 }
