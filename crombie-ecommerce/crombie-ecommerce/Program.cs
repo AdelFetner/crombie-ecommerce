5342832f5a0958780930680180975175c7271b57
Adel Fetner <adelfetner@gmail.com>
2025-01-03 18:09:38 -0300

    chore: fixed controller and updated migration

--- crombie-ecommerce/crombie-ecommerce/Program.cs
+++ crombie-ecommerce/crombie-ecommerce/Program.cs
@@ -1,4 +1,5 @@
 using crombie_ecommerce.Contexts;
+using crombie_ecommerce.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ProductService>();
 builder.Services.AddSqlServer<ShopContext>(builder.Configuration.GetConnectionString("DefaultConnection"));
 
 var app = builder.Build();
