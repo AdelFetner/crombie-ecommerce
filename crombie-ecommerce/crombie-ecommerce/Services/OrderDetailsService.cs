943edeb704fd0dbb9db076972980ea45a0cc96e1
GimEscalante <gimeescalante1@gmail.com>
2025-01-09 18:59:08 -0300

    chore: OrderDetails Service

--- crombie-ecommerce/crombie-ecommerce/Services/OrderDetailsService.cs
+++ crombie-ecommerce/crombie-ecommerce/Services/OrderDetailsService.cs
@@ -13,7 +13,40 @@ namespace crombie_ecommerce.Services
         }
 
         //create order detail
+        public async Task<OrderDetail> CreateDetails(OrderDetail detail)
+        {
+            _context.OrderDetails.Add(detail);
+            _context.SaveChanges();
+            return detail;
+        }
+
         //update order detail
+        public async Task<bool> UpdateDetails(Guid id, OrderDetail orderd)
+        {
+            var details = await _context.OrderDetails.FindAsync(id);
+            if (details == null) return false;
+
+            details.Quantity = orderd.Quantity;
+            details.Price = orderd.Price;
+            details.Subtotal = orderd.Subtotal;
+            
+            await _context.SaveChangesAsync();
+
+            return true;
+
+
+        }
+
+
         //delete order detail
-    }
+        public async Task DeleteDetail(Guid id)
+        {
+            var details = await _context.OrderDetails.FindAsync(id);
+            if (details != null)
+            {
+                _context.OrderDetails.Remove(details);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }   
 }
