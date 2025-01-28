aa92b816a994368de90a64208f2e7dd57b8ea381
GimEscalante <gimeescalante1@gmail.com>
2025-01-28 18:36:44 -0300

    feat:Roles

---
+++ crombie-ecommerce/crombie-ecommerce.BusinessLogic/RoleService.cs
@@ -0,0 +1,40 @@
+﻿using crombie_ecommerce.DataAccess.Contexts;
+using Microsoft.EntityFrameworkCore;
+using crombie_ecommerce.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crombie_ecommerce.BusinessLogic
+{
+    internal class RoleService
+    {
+        private readonly ShopContext _shopContext;
+
+
+        public RoleService(ShopContext shopContext)
+        {
+            _shopContext = shopContext;
+        }
+
+
+        public async Task<Role> GetDefaultRole()
+        {
+            return await _shopContext.Roles.FirstOrDefaultAsync(r => r.Name == "Cliente")
+                ?? throw new Exception("Rol por defecto no encontrado");
+        }
+
+        public async Task AssignRoleToUser(string username, int roleId)
+        {
+            var user = await _shopContext.Users.FirstOrDefaultAsync(u => u.Name == username);
+            if (user != null)
+            {
+                user.RoleId = roleId;
+                await _shopContext.SaveChangesAsync();
+            }
+        }
+    }
+}
